090a1eb6e39e65286822ac45f35f96fea50a1a9b
matthewwolfe <matthew.wolfe2@gmail.com>
2018-06-14 22:19:41 -0400

    add comment about applying mixins

--- src/utils/mixins.ts
+++ src/utils/mixins.ts
@@ -1,3 +1,9 @@
+/**
+ * Applies classes to a base class as mixins. Instance properties and methods will be merged with the base class, in order.
+ * The base class must provide typings for any properties and methods that are going to be mixed in.
+ * @param derivedCtor - base class
+ * @param baseCtors - mixin classes
+ */
 export function applyMixins(derivedCtor: any, baseCtors: any[]) {
     baseCtors.forEach(baseCtor => {
         Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
